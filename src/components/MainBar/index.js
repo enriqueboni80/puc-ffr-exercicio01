61d28e92b971678d8f062a235d80761f7999adab
rickboni80 <enriqueboni80@gmail.com>
2020-02-13 15:41:23 -0300

    Exercicio finalizado

---
+++ src/components/MainBar/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class MainBar extends Component {
+    render(){
+        return(
+            <div className="main-bar">
+                <ul>
+                    <li>Section 1 </li>
+                    <li>Section 2 </li>
+                    <li>Section 3 </li>
+                    <li>Section 4 </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default MainBar
---
+++ src/components/SideBar/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+class SideBar extends Component {
+    render(){
+        return(
+            <div className="side-bar">
+                <ul>
+                    <li>Section 1</li>
+                    <li>Section 2</li>
+                    <li>Section 3</li>
+                    <li>Section 4</li>
+                    <li>Section 5</li>
+                    <li>Section 6</li>
+                    <li>Section 7</li>
+                    <li>Section 8</li>
+                    <li>Section 9</li>
+                    <li>Section 10</li>
+                    <li>Section 11</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default SideBar
